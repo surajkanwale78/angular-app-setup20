3dffca4f70100054bf883d0f96bc5c3d9e656224
surajkanwale78 <surajkanwale@gmail.com>
2026-06-12 00:25:35 +0530

    Base setup done

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Child } from './child/child';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,Child],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-app-setup20');
+}
---
+++ src/app/child/child.ts
@@ -0,0 +1,11 @@
+import { Component,Input } from '@angular/core';
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.html',
+  styleUrl: './child.css',
+})
+export class Child {
+
+}
